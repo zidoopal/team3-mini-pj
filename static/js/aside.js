99e769c7b09a5b64b32ec7db5febcaca57e8f578
zidoopal <yuzzzin@gmail.com>
2023-08-11 00:39:50 +0900

    사이드바 JS 링크 이동 수정

--- static/js/aside.js
+++ static/js/aside.js
@@ -1,21 +1,16 @@
 'use strict';
 
-// 홈 (main)으로 이동
 const homeIcon = document.querySelector('#nav-main-button');
+const writeIcon = document.querySelector('#nav-write-button');
 
+// 홈 (main)으로 이동
 function homeHandler() {
-  // 메인 페이지 URL로 이동
-  window.location.href = 'http://plipli.site/';
+  window.location.href = '/';
 }
-
 homeIcon.addEventListener('click', homeHandler);
 
 // 작성 페이지 이동
-const writeIcon = document.querySelector('#nav-write-button');
-
 function writeHandler() {
-  // 작성 페이지 URL로 이동
-  window.location.href = 'http://plipli.site/create-post';
+  window.location.href = '/create-post';
 }
-
 writeIcon.addEventListener('click', writeHandler);
