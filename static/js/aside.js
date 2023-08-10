51261b31e6f4fe7461a313d9ea48f1fd0c16669d
zidoopal <yuzzzin@gmail.com>
2023-08-10 21:57:07 +0900

    (수정) if문 삭제

--- static/js/aside.js
+++ static/js/aside.js
@@ -4,20 +4,18 @@
 const homeIcon = document.querySelector('#nav-main-button');
 
 function homeHandler() {
-  if (true) {
-    window.location.href = 'http://plipli.site/';
-  }
+  // 메인 페이지 URL로 이동
+  window.location.href = 'http://plipli.site/';
 }
+
 homeIcon.addEventListener('click', homeHandler);
 
 // 작성 페이지 이동
 const writeIcon = document.querySelector('#nav-write-button');
 
 function writeHandler() {
-  if (true) {
-    // 작성 도메인 아직??
-    window.location.href = 'http://plipli.site/create-post';
-  }
+  // 작성 페이지 URL로 이동
+  window.location.href = 'http://plipli.site/create-post';
 }
 
 writeIcon.addEventListener('click', writeHandler);
