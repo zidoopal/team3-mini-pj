9d8e4b33a68dc8d4b796db481628560912102714
wynter24 <m.wynter.k@gmail.com>
2023-08-10 17:28:48 +0900

    게시글 작성 페이지

--- static/js/create-post.js
+++ static/js/create-post.js
@@ -1,3 +1,21 @@
+const uploadBtn = document.querySelector("#upload_button");
+
 function upload() {
-  
+  let image = document.querySelector("#image_upload").files[0];
+  // console.log(image)
+  let song_title = document.querySelector("#song_title").value;
+  let artist = document.querySelector("#artist").value;
+  let formData = new FormData();
+  formData.append("image_give", image);
+  formData.append("song_title_give", song_title);
+  formData.append("artist_give", artist);
+
+  fetch("/upload", { method: "POST", body: formData })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data["msg"]);
+      window.location.reload();
+    });
 }
+
+uploadBtn.addEventListener("click", upload);
